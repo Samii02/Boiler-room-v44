8499d94eb5a7662a37abab3498b7f2ed95a22091
Sami A <alhalabisami@gmail.com>
2024-11-03 16:56:37 +0100

    added a alert message to the show task function

--- script.js
+++ script.js
@@ -35,6 +35,13 @@ function showTasks() {
     tasks.forEach(task => {
         console.log(`ID: ${task.id}, Beskrivning: ${task.description}, Klar: ${task.done ? "Ja" : "Nej"}`)
     })
+    
+    let message = "Uppgifter:\n";
+    tasks.forEach(task => {
+        message += `ID: ${task.id}, Beskrivning: ${task.description}, Klar: ${task.done ? "Ja" : "Nej"}\n`;
+    });
+
+    alert(message);
 }
 
 function showMenu() {
