2b20b36bca552186c11ca90730b89318cd3201d9
Sami A <alhalabisami@gmail.com>
2024-11-03 16:24:57 +0100

    addad menu function and added link between

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function addTask() {
     };
     tasks.push(newTask);
     alert("Uppgift tillagd!")
+    console.log(newTask);
 }
 
 function generateId() {
@@ -21,5 +22,33 @@ function generateId() {
     for (let i = 0; i < 8; i++) {
         id += characters.charAt(Math.floor(Math.random() * characters.length));        
     }
+    console.log(id)
     return id;
 }
+
+function showMenu() {
+    let choice;
+    do {
+        choice = prompt("Välj ett alternativ:\n1: Lägg till en ny uppgift\n2: Visa alla uppgifter\n3: Markera en uppgift som klar\n4: Ta bort en uppgift\n5: Avsluta programmet");
+        switch (choice) {
+            case '1':
+                addTask();
+                break;
+            case '2':
+                null;
+                break;
+            case '3':
+                null
+                break;
+            case '4':
+                null
+                break;
+            case '5':
+                alert("Programmet avslutas.")
+                break;
+        }
+    }
+    while (choice !== '5');
+}
+
+showMenu();
